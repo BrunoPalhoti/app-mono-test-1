4bdb9b47a182ccf559fd4439f9c3ce12c4ab8181
Bruno Palhoti <43323404+BrunoPalhoti@users.noreply.github.com>
2025-08-16 15:36:56 -0300

    refactor: remove ponto e vírgula do código para consistência

--- packages/api-node/controllers/userController.js
+++ packages/api-node/controllers/userController.js
@@ -1,8 +1,8 @@
-let users = [];
+let users = []
 
-export const getUsers = (req, res) => res.json(users);
+export const getUsers = (req, res) => res.json(users)
 
 export const createUser = (req, res) => {
-  users.push(req.body);
-  res.status(201).json(req.body);
-};
+  users.push(req.body)
+  res.status(201).json(req.body)
+}
