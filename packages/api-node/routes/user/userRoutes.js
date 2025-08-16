7ca25a99b46ec0fd2f727703c3b8a2c87aa0c7d0
Bruno Palhoti <43323404+BrunoPalhoti@users.noreply.github.com>
2025-08-16 17:43:25 -0300

    feat: implementação da funcionalidade de atualização de usuário e remoção de código obsoleto

--- packages/api-node/routes/user/userRoutes.js
+++ packages/api-node/routes/user/userRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { getUsers } from '../../controllers/user/get/get-user.js'
 import { createUser } from '../../controllers/user/post/create-user.js'
+import { updateUser } from '../../controllers/user/patch/update-user.js'
 
 const router = Router()
 
 router.get('/getUsers', getUsers)
 router.post('/createUser', createUser)
+router.patch('/updateUser/:id', updateUser)
 
 export default router
