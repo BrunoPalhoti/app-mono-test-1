b3f4d2b4a7dbd5b2c7cdf1ba5184dcc0036a2f31
Bruno Palhoti <43323404+BrunoPalhoti@users.noreply.github.com>
2025-08-16 19:00:29 -0300

    feat: adiciona autenticação JWT e rota de login

--- packages/api-node/app.js
+++ packages/api-node/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import userRoutes from './routes/user/userRoutes.js'
 import postsRoutes from './routes/posts/postsRoutes.js'
+import authRoutes from './routes/auth/authRoutes.js'
 
 const app = express()
 app.use(express.json())
 app.use('/users', userRoutes)
 app.use('/posts', postsRoutes)
+app.use('/auth', authRoutes)
 
 export default app
--- packages/api-node/routes/posts/postsRoutes.js
+++ packages/api-node/routes/posts/postsRoutes.js
@@ -2,11 +2,12 @@ import { Router } from 'express'
 import { getPosts } from '../../controllers/posts/get/get-posts.js'
 import { createPost } from '../../controllers/posts/post/create_post.js'
 import { likePost } from '../../controllers/posts/post/like-post.js'
+import { authenticate } from '../../auth/authenticate.js'
 
 const router = Router()
 
 router.get('/', getPosts)
 router.post('/', createPost)
-router.post('/:id/like', likePost)
+router.post('/:id/like', authenticate, likePost)
 
 export default router
