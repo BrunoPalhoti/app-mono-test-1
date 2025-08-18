e80d0be9c8494ba1237b8dfdc6542b816973e341
Bruno Palhoti <43323404+BrunoPalhoti@users.noreply.github.com>
2025-08-18 14:01:35 -0300

    feat: atualizar comando de inicialização no Dockerfile e implementar busca de usuários no controlador

--- packages/api-node/controllers/user/get/get-user.js
+++ packages/api-node/controllers/user/get/get-user.js
@@ -1,5 +1,11 @@
-import { mockGetUsers } from '../../../mock/mock-get-user.js'
+import pool from '../../../models/db.js'
 
-export const getUsers = (req, res) => {
-  return res.json(mockGetUsers())
+export const getUsers = async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users')
+    return res.json(result.rows)
+  } catch (error) {
+    console.log('Error fetching users:', error)
+    return res.status(500).json({ error: 'Erro ao buscar usuários' })
+  }
 }
